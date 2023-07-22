56854233fbaba02450285c934221dea2cb0e05b7
SIDDHHARTH <79316753+siddharth7901@users.noreply.github.com>
2023-07-22 09:56:43 +0530

    Add files via upload

---
+++ SwapNumberss.java
@@ -0,0 +1,22 @@
+
+// 7 how to swapp 2 no. without using 3rd variable 
+import java.util.Scanner;
+
+class SwapNumberss {
+    public static void main(String args[]) {
+        int x, y;
+        System.out.println("Enter x and y");
+        Scanner in = new Scanner(System.in);
+
+        x = in.nextInt();
+        y = in.nextInt();
+
+        System.out.println("Before Swapping\nx= " + x + "\ny =" + y);
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("After Swapping\nx = " + x + "\ny = " + y);
+    }
+}
