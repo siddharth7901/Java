d7364058a2167bbcad96e09fa5958bc465876dcd
SIDDHHARTH <79316753+siddharth7901@users.noreply.github.com>
2023-07-25 20:19:40 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class WhileLoop{
+    public static void main(String[] args){
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input an integer");
+
+        while((n= input.nextInt()) !=0){
+            System.out.println("you entered " + n);
+            System.out.println("Input an integer");
+        }
+        System.out.println("Out of loop");
+    }
+ }
