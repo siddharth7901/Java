880c2883348ac96f525260e7d373864401fc34e0
SIDDHHARTH <79316753+siddharth7901@users.noreply.github.com>
2023-07-25 20:20:58 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,13 @@
+public class Star {
+    public static void main(String[] args){
+        int row, numberOfStar;
+
+        for(row =1; row <=10; row ++){
+            for(numberOfStar =1; numberOfStar<=row; numberOfStar++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
